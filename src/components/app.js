8bb3bd104461332e4a552666decb9e4b17f7e6b9
Dimsol <claudiosakaeshigemi@gmail.com>
2018-08-01 20:59:26 -0300

    aula 49 corrigido

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Component } from 'react';
-
 import BookList from '../containers/book-list';
 import BookDetail from '../containers/book-detail';
 
--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -2,7 +2,7 @@
 //this reducer is responsible for
 
 export default function( state = null, action ) {
-    switch(action.tipe) {
+    switch(action.type) {
         case 'BOOK_SELECTED':
             return action.payload;
     }
